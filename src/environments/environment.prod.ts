04c03f6fae850cd957b1c48f062b4528498614a8
Renato Barros <renatobarros123@outlook.com.br>
2021-11-23 23:30:05 -0300

    update environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  BASE_API_URL: 'https://gateway.marvel.com/',
+  TS: 'ts=1',
+  API_KEY: 'apikey=ec023cc710cb69c813951883df7c163c',
+  HASH: 'hash=959415f635fc4a7430a78298e79d4eae',
+  production: true,
 };
